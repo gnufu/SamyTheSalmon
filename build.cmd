c1c6a894295ac2e88c11f92e6a58050fa816b6d2
Lisa Kellner <kellner@vrvis.at>
2017-10-02 11:11:21 +0200

    update packages

--- build.cmd
+++ build.cmd
@@ -8,15 +8,15 @@ if errorlevel 1 (
   exit /b %errorlevel%
 )
 
-.paket\paket.exe restore group Build
+if NOT exist paket.lock (
+    echo No paket.lock found, running paket install.
+    .paket\paket.exe install
+)
+
+.paket\paket.exe restore --group Build
 if errorlevel 1 (
   exit /b %errorlevel%
 )
 
-cls
-
 SET FSI_PATH=packages\build\FAKE\tools\Fake.exe
 "%FSI_PATH%" "build.fsx" Dummy --fsiargs build.fsx --shadowcopyreferences+ %* 
-
-
-
